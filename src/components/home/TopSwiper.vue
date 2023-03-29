d26833af63ae3ddb97bf7823401874cf54612f36
yinqiyao <yinqiyao99@foxmail.com>
2023-03-30 01:37:50 +0800

    首页轮播图

---
+++ src/components/home/TopSwiper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <van-swipe :autoplay="3000" indicator-color="#d43c33">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+<script>
+export default {
+  name: "TopSwiper",
+  setup() {
+    const images = [
+      'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
+      'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg',
+    ];
+    return { images };
+  }
+};
+</script>
+<style scoped lang="less">
+.van-swipe {
+  width: 100%;
+  height: 3rem;
+  .van-swipe-item {
+    padding:  0 0 .2rem;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <top-nav></top-nav>
+    <top-swiper></top-swiper>
   </div>
 </template>
 
 <script>
 import TopNav from '@/components/home/TopNav.vue'
+import TopSwiper from '@/components/home/TopSwiper.vue'
 export default {
   name: 'Home',
   components: {
