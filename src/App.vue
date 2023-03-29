0ef523f5705eb5c42b011da3e834b169bd6289cb
yinqiyao <yinqiyao99@foxmail.com>
2023-03-30 01:13:59 +0800

    引入iconfont

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,16 @@
 </template>
 
 <style lang="less">
-
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.icon {
+  width: .4rem; 
+  height: .4rem;
+  vertical-align: -0.08rem;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
